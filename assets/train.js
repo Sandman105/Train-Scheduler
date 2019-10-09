342ce9062a459640a51907991bfa9cd6df85a896
Sandman105 <cpossandon@gmail.com>
2019-10-09 01:02:22 -0400

    basic HTML completed, firebase config variable defined

---
+++ assets/train.js
@@ -0,0 +1,16 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyC70r8vIlqpDVryfO9F3b7pVlV9K9PgxtQ",
+    authDomain: "train-schedule-f9a80.firebaseapp.com",
+    databaseURL: "https://train-schedule-f9a80.firebaseio.com",
+    projectId: "train-schedule-f9a80",
+    storageBucket: "",
+    messagingSenderId: "892687017487",
+    appId: "1:892687017487:web:1a1ecd947cb1fb2101657a",
+    measurementId: "G-NKSZTF31P7"
+  };
+
+  firebase.initializeApp(firebaseConfig);
+
+  var database = firebase.database();
+
+  
