15555e4aca57f65141ec6826677df16b65be3fcd
Sandman105 <cpossandon@gmail.com>
2019-10-10 00:39:27 -0400

    added child_added event to retrieve db items

--- assets/train.js
+++ assets/train.js
@@ -15,25 +15,13 @@ firebase.initializeApp(firebaseConfig);
 
 var trainDatabase = firebase.database();
 
-//creating objects
-//clearing the form
-//.spilt to enter the time correctly (":")
-
-
-
-
-
-
-
-
-
 
 
 
 //CLICK FUNCTION===============================================================
 
 $("#submit-train").on("click", function (event) {
-    
+
     event.preventDefault();
     console.log("Test submit: " + event);
 
@@ -69,17 +57,34 @@ $("#submit-train").on("click", function (event) {
     console.log(newTrainEmpty.frequency);
     console.log(newTrainEmpty.trackLocation);
 
-
-
-
-
-
-
-
-
-
-
-
+    //As we did in other examples in our activities, clear out the values that were entered, so start a new set of values. Tested, values cleared after submitting new values.
+    $("#train-name").val("");
+    $("#train-destination").val("");
+    $("#first-train-time").val("");
+    $("#train-frequency").val("");
+    $("#train-track").val("");
 
 });
 
+//child_added event is typically used when retrieving a list of items from the database. Checking the console.log, seeing the items that were already added, returned.
+trainDatabase.ref().on("child_added", function (dbSnapshot) {
+    console.log("---------------")
+    console.log(dbSnapshot.val());
+
+    var trainName = dbSnapshot.val().name;
+    var trainDestination = dbSnapshot.val().location;
+    var firstTrainTime = dbSnapshot.val().newArrival;
+    var trainFrequency = dbSnapshot.val().frequency;
+    var trainTrack = dbSnapshot.val().trackLocation;
+
+    console.log(trainName);
+    console.log(trainDestination);
+    console.log(firstTrainTime);
+    console.log(trainFrequency);
+    console.log(trainTrack);
+
+
+
+
+
+});   
